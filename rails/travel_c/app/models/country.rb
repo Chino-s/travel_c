5eb40e528b19c46a2b55a914e3c01a86db2367e0
Chino-s <s.chino.hightwo@gmail.com>
2017-05-22 13:46:18 +0900

    master<=db

---
+++ rails/travel_c/app/models/country.rb
@@ -0,0 +1,13 @@
+class Country < ActiveRecord::Base
+  attr_accessible :name, :language, :capital, :area_id, :popular_ranking
+
+  class << self
+      def search(query)
+        rel = order("id")
+        if query.present?
+          rel = rel.where("name LIKE ?",
+                  "%#{query}%")
+        end
+      rel
+   end
+end
