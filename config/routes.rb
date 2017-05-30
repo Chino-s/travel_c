3be66359965c9dd7ccc5d70eb60aae06d5a961cd
Chino-s <s.chino.hightwo@gmail.com>
2017-05-30 12:59:03 +0900

    [#16]routesファイル整理(修正)

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 TravelC::Application.routes.draw do
 
-  resources :top, :only => [:index]
-  root :controller => 'top', :action => 'index', as: "top"
+  get "top" => "top#index", as: "top"
+  root to: "top#index"
 
-  get "/countries/:id", to: "countries#show", as: "countries"
-  resources :countries, :only => [:index, :show], as: "countries" do
+  get "/countries/:id", to: "countries#show", as: "country"
+  resources :countries do
     collection { get "search" }
   end
 
   get "/cities/:id", to: "cities#show", as: "city"
-  resources :cities, :only => [:index, :show] do
+  resources :cities do
     collection { get "search" }
   end
 
   get "/foods/:id", to: "foods#show", as: "food"
-  resources :foods, :only => [:index, :show] do
+  resources :foods do
     collection { get "search" }
   end
 
