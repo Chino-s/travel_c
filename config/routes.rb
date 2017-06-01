82a2de908a3b10724cb7969829e4ec0757d6ee77
Chino-s <s.chino.hightwo@gmail.com>
2017-06-01 15:49:26 +0900

    [#16]routesレビュー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 TravelC::Application.routes.draw do
 
-
-  resources :top, :only => [:index], as: "top"
   root to: "top#index"
 
   resources :countries, :only => [:index, :show] do
     collection { get "search" }
   end
-
 
   resources :cities, :only => [:index, :show] do
     collection { get "search" }
