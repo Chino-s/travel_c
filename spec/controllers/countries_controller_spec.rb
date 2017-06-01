5e86dfeef1f07c0da79a1413004c8f6061a8ba31
Chino-s <s.chino.hightwo@gmail.com>
2017-06-01 18:21:18 +0900

    [#21]countries_controllerのindexテストを追加

--- spec/controllers/countries_controller_spec.rb
+++ spec/controllers/countries_controller_spec.rb
@@ -2,17 +2,31 @@ require 'rails_helper'
 
 RSpec.describe CountriesController, :type => :controller do
 
-    describe "GET 'show'" do
-      it "should returns http success" do
-        get 'show'
-        response.should be_success
-      end
+  describe  'GET #index' do
 
-      it "should assigns @id" do
-        get 'show'
-        assigns[:id].should_not be_nil
-      end
-
+    it 'show view' do
+      get :index
+      expect(response).to render_template(:index)
     end
+
+    context 'have data' do
+      before :each do  #'add data in table'
+        @countries1 = FactoryGirl.create(:country)
+        @countries2 = FactoryGirl.create(:country)
+      end
+        it 'check order data' do
+          get :index
+          expect(assigns[:countries]).to eq([@countries1, @countries2])
+        end
+    end
+
+    context 'no have data' do
+      it 'countries is empty array' do
+        get :index
+        expect(assigns[:countries]).to eq([])
+      end
+    end
+
+  end
 
 end
---
+++ spec/factories/country.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :country do
+    area_id 1
+    capital "a"
+    googlemap "123"
+    imagephoto "aaa"
+    language "abc"
+    name "aabbcc"
+    popular_ranking 4
+  end
+end
